c6cd61fe13d11b7c36f6681eae7d9b94df8eb9bb
matnex-mix <jolaoshobatmat@gmail.com>
2021-07-26 21:41:41 +0100

    Added absolute static root...

--- techie/settings.py
+++ techie/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
