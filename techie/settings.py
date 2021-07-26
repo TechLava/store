59d30540e3cc8e3c5a5809567935d0699441771c
matnex-mix <jolaoshobatmat@gmail.com>
2021-07-26 21:37:30 +0100

    Added absolute static root...

--- techie/settings.py
+++ techie/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import dj_database_url
+import os
 
 from pathlib import Path
 
@@ -123,7 +124,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = 'staticfile/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
